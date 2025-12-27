cce4efae7b047575a2c10b1a48989b6c12abfc16
Kehvarl <Kehvarl@Kehvarl.com>
2025-12-26 20:15:51 -0700

    throw it on the screen

--- app/main.rb
+++ app/main.rb
@@ -7,22 +7,19 @@ class Switch_Display
   def initialize vars={}
     @x = vars.x || 640
     @y = vars.y || 360
-    @w = 64
-    @h = 96
-    setup_display()
+    @w = 256
+    @h = 192
+    @display = SevenSegmentDisplay.new({x:@x, y:@y+96, w:@w, h:96})
     @switches = create_switchline(4)
     @leds = create_leds (4)
-  end
 
-  def setup_display
-    @display = SevenSegmentDisplay.new({})
   end
 
   def create_switchline count
     line = []
-    start = (720 - (count * 48)).div(2)
+    start = (640 - (count * 48)).div(2)
     (0...count).each do |x|
-      line << Toggle_Switch.new({x:x*50 + start,y:640, w:48, h:96})
+      line << Toggle_Switch.new({x:x*50 + start,y:@y+32, w:48, h:96})
     end
     line
   end
@@ -36,29 +33,25 @@ class Switch_Display
   end
 
   def make_led index, color
-    total_gaps = (@segments - 1) * @led_spacing
+    total_gaps = (4 - 1) * 3
     spacing = @w - total_gaps
-    segment_w = spacing.to_f / @segments
-    x = index * (segment_w + @led_spacing) + (segment_w - @led_w) / 2.0
-    y = (@h - @led_h) / 2
-    {x:x, y:y, w:@led_w, h:@led_h, path: "sprites/led_gs.png", **color}.sprite!
+    segment_w = spacing.to_f / 4
+    x = index * (segment_w + 3) + (segment_w - 64) / 2.0
+    y = (@h - 64) / 2
+    {x:x, y:y, w:64, h:64, path: "sprites/led_gs.png", **color}.sprite!
   end
 
   def render
     out = []
     out << @display.render
-    out << @switchline
-    out << @ledline
+    out << @switches
+    out << @leds
+    out
   end
 end
 
 def init args
-  args.state.game_over = false
-  args.state.won = false
-  args.state.button = Pushbutton.new({x:500, y:640, w:96, h:96, source_w:64, source_h:32})
-  args.state.display = Display.new()
-  args.state.timer = Timer.new({x:160, y:800, w:384, h:144, time:20.0})
-  set_switches(args, 1)
+  args.state.test = Switch_Display.new()
 end
 
 def set_switches(args, count)
@@ -106,52 +99,13 @@ def calculate args
   return output
 end
 
-def game_over_tick args
-    args.outputs.primitives << {x:0, y:0, w:720, h:1280, r:0, g:0, b:0}.solid!
-    args.outputs.primitives << {x:280, y:800, w:50, h:50, r:0, g:196, b:0, size_enum: 30, text:"GAME"}.label!
-    args.outputs.primitives << {x:280, y:700, w:50, h:50, r:0, g:196, b:0, size_enum: 30, text:"OVER"}.label!
-    if not args.state.won
-      args.outputs.primitives << {x:255, y:600, w:50, h:50, r:255, g:196, b:0, size_enum: 16, text:"You Lose!"}.label!
-    else
-      args.outputs.primitives << {x:255, y:600, w:50, h:50, r:255, g:196, b:0, size_enum: 16, text:"You Won!"}.label!
-    end
-    args.outputs.primitives << {x:230, y:300, w:260, h:80, r:0, g:196, b:0}.solid!
-    args.outputs.primitives << {x:260, y:370, w:80, h:80, r:0, g:0, b:0, size_enum: 20, text:"RESTART"}.label!
-
-    if args.inputs.mouse.button_left and args.inputs.mouse.inside_rect?({x:320, y:300, w:80, h:80})
-      init args
-    end
-end
 
 def tick args
   if Kernel.tick_count <= 0
       init args
   end
-  if args.state.game_over
-    game_over_tick(args)
-    return
-  end
 
-  args.state.switches.each {|s| s.tick(args)}
-  args.state.button.tick(args)
-  args.state.timer.tick(args)
+  args.outputs.primitives << {x:0, y:0, w:1280, h:720, r:0, g:0, b:0}.solid!
 
-  if args.state.timer.ended
-        args.state.game_over = true
-  end
-
-  if args.state.button.status > 0
-      calculate(args)
-      args.state.button.status = 0
-  end
-
-  args.outputs.primitives << {x:0, y:0, w:720, h:1280, r:0, g:0, b:0}.solid!
-  args.outputs.primitives << {x:260, y:1280, w:720, h:280,size_enum:36, text:"Stop",r:255, g:196, b:0}.label
-  args.outputs.primitives << {x:290, y:1200, w:720, h:280,size_enum:36, text:"The",r:255, g:196, b:0}.label
-  args.outputs.primitives << {x:250, y:1120, w:720, h:280,size_enum:36, text:"Timer",r:255, g:196, b:0}.label
-  args.outputs.primitives << args.state.switches
-
-  args.outputs.primitives << args.state.timer.render
-  args.outputs.primitives << args.state.display.render
-  args.outputs.primitives << args.state.button
+  args.outputs.primitives << args.state.test.render
 end
